c613b1123f85ae3cb18a7e4cbe9cb73d781fbb7a
Yoriki Yamaguchi <yamaguchi@cloudpack.jp>
2017-07-04 01:39:11 +0900

    fix formula

--- homebrew/awsprice.rb
+++ homebrew/awsprice.rb
@@ -5,7 +5,7 @@ class Awsprice < Formula
 
   homepage "https://github.com/y13i/awsprice"
   version  VERSION
-  url      "https://github.com/y13i/awsprice/releases/download/#{VERSION}/awsprice-darwin_amd64.zip"
+  url      "https://github.com/y13i/awsprice/releases/download/v#{VERSION}/awsprice-darwin_amd64.zip"
   sha256   "1a187c1dd965b8f4b62de09546caf7623017da7cce43072121605a2187616b37"
 
   head "https://github.com/y13i/awsprice.git", :branch => "master"
